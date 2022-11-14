d853ef7a4a4ea15cb51cc32b10a35802cc3119de
luigigianlupi <luigi.gianlupi@ict-group.it>
2022-11-14 16:58:21 +0100

    config refresh native OK

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo -e "------------------------------------"
+echo -e "./mvnw package -Pnative -Dquarkus.native.container-build=true"
+echo -e "------------------------------------"
+./mvnw package -Pnative -Dquarkus.native.container-build=true
+echo -e "------------------------------------"
+echo -e "docker build -f src/main/docker/Dockerfile.native -t quarkus-open-telemetry ."
+docker build -f src/main/docker/Dockerfile.native -t quarkus-open-telemetry .
+echo -e "------------------------------------"
+
+
+
