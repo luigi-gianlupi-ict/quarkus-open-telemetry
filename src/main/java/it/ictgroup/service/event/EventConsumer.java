896c64cb6f4be186fddce0c8917bc7cc54c57959
luigigianlupi <luigi.gianlupi@ict-group.it>
2022-11-29 10:24:13 +0100

    incoming -> outgoing

--- src/main/java/it/ictgroup/service/event/EventConsumer.java
+++ src/main/java/it/ictgroup/service/event/EventConsumer.java
@@ -2,11 +2,16 @@ package it.ictgroup.service.event;
 
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 import it.ictgroup.model.pojo.CustomObject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 /**
  * A bean consuming data from the "event" Kafka topic and applying some conversion.
@@ -18,12 +23,15 @@ public class EventConsumer {
     final Logger LOG = Logger.getLogger(getClass());
 
     @Incoming("event-in")
-    //@Outgoing("my-data-stream")
+    @Outgoing("my-data-stream")
     //@Broadcast
     public CustomObject process(CustomObject customObject) {
         LOG.infof("Custom Object %s %s", customObject.getName(),customObject.getSurname());
         customObject.setName("Mario");
         return customObject;
     }
+
+
+
 
 }
--- src/main/java/it/ictgroup/service/event/EventSubscriber.java
+++ src/main/java/it/ictgroup/service/event/EventSubscriber.java
@@ -1,61 +1,20 @@
 package it.ictgroup.service.event;
 
-import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.StartupEvent;
 import it.ictgroup.model.pojo.CustomObject;
-import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 
 @ApplicationScoped
 public class EventSubscriber {
 
     final Logger LOG = Logger.getLogger(getClass());
 
-    @Inject
-    @Channel("my-data-stream")
-    Publisher<CustomObject> customObjectPublisher;
-
-
-    void onStart(@Observes StartupEvent ev) {
-        //customObjectPublisher.subscribe(customObjectSubscriber);
-        LOG.info("The application is starting...");
+    @Incoming("my-data-stream")
+    public void consume(CustomObject customObject) {
+        LOG.info("[Subscriber] : " + Thread.currentThread().getName() + " event : " + customObject.getName());
     }
 
-    void onStop(@Observes ShutdownEvent ev) {
-        LOG.info("The application is stopping...");
-    }
-
-    Subscriber<CustomObject> customObjectSubscriber = new Subscriber<>() {
-        private Subscription subscription;
-
-        @Override
-        public void onSubscribe(Subscription subscription) {
-            this.subscription = subscription;
-            this.subscription.request(1);
-        }
-
-        @Override
-        public void onNext(CustomObject customObject) {
-            System.out.println("[Subscriber] : " + Thread.currentThread().getName() + " event : " + customObject.getName());
-            this.subscription.request(1);
-        }
-
-        @Override
-        public void onError(Throwable throwable) {
-        }
-
-        @Override
-        public void onComplete() {
-            System.out.println("onComplete");
-        }
-    };
 
 
 
